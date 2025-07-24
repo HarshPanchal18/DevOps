2af5f0dc3e32a07f143a13b00aced4a06ab4df8c
Harsh Panchal <harshhhh1803@gmail.com>
2025-07-24 11:08:12 +0530

    Allowed ssh traffic

--- terraform/aws/ec2/providers.tf
+++ terraform/aws/ec2/providers.tf
@@ -1,5 +1,10 @@
 provider "aws" {
+  alias = "keys"
   access_key = var.AWS_IAM_ACCESS_KEY # coming from secret.tf
   secret_key = var.AWS_IAM_SECRET_KEY # coming from secret.tf
-  region = var.AWS_REGION
+}
+
+provider "aws" {
+  alias = "west"
+  region = "us-west-2" # Use the AWS provider for us-west-2
 }
--- terraform/aws/ec2/resources.tf
+++ terraform/aws/ec2/resources.tf
@@ -1,4 +1,25 @@
 resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  provider = aws.east # Use the AWS provider for us-east-1
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name = "Allow ssh"
+  description = "Allow ssh inbound traffic"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere
+  }
+
+  egress {
+    from_port = 0 # 0 means all ports
+    to_port = 0
+    protocol = "-1" # -1 means all protocols
+    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
+  }
 }
