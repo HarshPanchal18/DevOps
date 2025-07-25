9ef7bffb9872042f48f6077f2626919ffa8ff24b
Harsh Panchal <harshhhh1803@gmail.com>
2025-07-25 16:49:11 +0530

    Provided storage block, tags, lifecycle, http-ingress to ec2

--- terraform/aws/ec2/resources.tf
+++ terraform/aws/ec2/resources.tf
@@ -1,25 +1,51 @@
 resource "aws_instance" "example" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-  provider = aws.east # Use the AWS provider for us-east-1
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+  provider               = aws.east # Use the AWS provider for us-east-1
+  # key_name = aws_keypair.my_key.key_name
+
+  root_block_device {
+    volume_size = var.volume_size
+    volume_type = "gp2" # General Purpose SSD
+  }
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name = "Allow ssh"
-  description = "Allow ssh inbound traffic"
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "Allow ssh & http"
+  description = "Allow ssh & http inbound-outbound traffic"
 
+  # Inbound rules
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    description = "Allow SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere
   }
 
+  ingress {
+    description = "Allow HTTP access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere
+  }
+
+  # Outbound rules
   egress {
-    from_port = 0 # 0 means all ports
-    to_port = 0
-    protocol = "-1" # -1 means all protocols
+    description = "Allow all outbound traffic"
+    from_port   = 0 # 0 means all ports
+    to_port     = 0
+    protocol    = "-1" # -1 means all protocols
     cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
   }
 }
--- terraform/aws/ec2/vars.tf
+++ terraform/aws/ec2/vars.tf
@@ -9,3 +9,7 @@ variable "instance_type" {
 variable "ami_id" {
   default = "ami-0c02fb55956c7d316"
 }
+
+variable "volume_size" {
+  default = 8
+}
