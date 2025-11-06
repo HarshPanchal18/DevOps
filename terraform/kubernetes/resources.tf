a8dff88586ac1f8ef252b97a82acd0ebb4cd05b5
Harsh Panchal <harshhhh1803@gmail.com>
2025-11-06 16:56:32 +0530

    Applied deployment, service for nginx

--- terraform/kubernetes/resources.tf
+++ terraform/kubernetes/resources.tf
@@ -1,15 +1,60 @@
 resource "kubernetes_pod" "nginx" {
     metadata {
-        name = "nginx-example"
-        labels = {
+        name    = "nginx-example"
+        labels  = {
             app = "nginx"
         }
     }
-
     spec {
         container {
-            iamge = "nginx:1.19"
+            image = "nginx:1.19"
             name  = "nginx"
+        }
+    }
+}
+
+resource "kubernetes_deployment" "nginx-deploy" {
+    metadata {
+        name    = "nginx-deployment"
+        labels  = {
+            app = "nginx-deploy"
+        }
+    }
+    spec {
+        replicas = 3
+        selector {
+            match_labels = {
+                app      = "nginx-deploy"
+            }
+        }
+        template {
+            metadata {
+                labels  = {
+                    app = "nginx-deploy"
+                }
+            }
+            spec {
+                container {
+                    image = "nginx:1.19"
+                    name  = "nginx"
+                }
+            }
+        }
+    }
+}
+
+resource "kubernetes_service" "nginx" {
+    metadata {
+        name = "svc-nginx"
+    }
+    spec {
+        selector = {
+            app = "nginx-deploy"
+        }
+        type = "NodePort"
+        port {
+            port       = 80
+            target_port = 80
         }
     }
 }
