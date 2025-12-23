b075cc2a6ce4f4219b8067ab6ae2f66b528b00bd
Harsh Panchal <harshhhh1803@gmail.com>
2025-12-23 14:49:21 +0530

    Created tf for AWS S3

---
+++ cloud/go/aws/s3/terraform/vars.tf
@@ -0,0 +1,19 @@
+variable "access_key" {
+    type = string
+}
+
+variable "secret_key" {
+    type = string
+}
+
+variable "region" {
+    type = string
+}
+
+variable "s3_endpoint" {
+    type = string
+}
+
+variable "s3_bucket_name" {
+    type = string
+}
